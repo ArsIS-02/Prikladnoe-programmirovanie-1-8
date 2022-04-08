f7a48961bd177e20ee58e26092557682c57a04af
ArsIS-02 <74455553+ArsIS-02@users.noreply.github.com>
2022-04-08 22:17:52 +0500

    Add files via upload

---
+++ 1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru"); // русская кодировка в командной строке
+    int i, k;
+    int n;
+    cout << "Введите основание: ";
+    cin >> n;           //ввод основания 
+    int power(int, int);//описание прототипа метода  
+
+
+    for (i = 0; i < 10; i++)//в цикле меняется показатель степени от 0 до 9 
+    {
+        k = power(n, i);              //в метод передаем основание и степень 
+        cout << n << "^" << i << "=" << k << endl; //выводим результат в терминал
+    }
+}
+int power(int x, int n)//метод с двумя параметрами 
+{
+    int i, p = 1;
+    for (i = 1; i <= n; i++) p *= x;//произведение основания n раз на себя 
+    return p;                   //возвращаем результат 
+    system("pause>>void");
+}
---
+++ 1/ConsoleApplication2_1/ConsoleApplication2_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> 
+#define PR(x)  printf("x=%u, *x=%f, &x=%u \n",x,*x, &x) 
+#include <stdio.h> 
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru"); // русская кодировка в командной строке
+    float mas[] = { 465.8, 279.2, 804.5 }; //объявлен массив 
+    float* ptrl, * ptr2; //объявлены указатели 
+    ptrl = mas; //указатель на массив 
+    ptr2 = &mas[2]; //указатель адрес массива 
+    PR(ptrl); //вывод значения, указателя на значение, адрес 
+    ptrl++; //переход к следующему элементу указателя 
+    PR(ptrl);
+    PR(ptr2);
+    ++ptr2;
+    PR(ptr2);
+    printf("ptr2-ptrl=%d\n", ptr2 - ptrl);
+    system("pause>>void");
+}
